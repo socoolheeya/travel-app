58cb4b982be64b6d8441524ead53e75c14d18dcb
wonhee.lee <lwh0102@gmail.com>
2024-06-24 00:33:17 +0900

    remove existing travel-app-internal-api from index

--- travel-app-internal-api/src/main/java/com/socoolheeya/travel/app/internal/api/core/property/intrastructure/PropertyPersistenceService.java
+++
@@ -1,7 +0,0 @@
-package com.socoolheeya.travel.app.internal.api.core.property.intrastructure;
-
-import com.socoolheeya.travel.domain.rds.main.property.entity.PropertyEntity;
-
-public interface PropertyPersistenceService {
-    PropertyEntity findById(Long propertyId);
-}
